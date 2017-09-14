fa8133414e755757845f48800ca2386a794c637a
hardikkhurana19 <hardikkhurana19@.github.com>
2017-09-14 23:02:12 +0530

    color codding in main file

--- Spy_Chat/main.py
+++ Spy_Chat/main.py
@@ -4,15 +4,21 @@ from default_user import details
 from choices import choices
 from termcolor import colored, cprint
 
-
-cprint("Let's Get Started!!", "cyan", attrs=['dark', 'underline', 'bold'])
-choice = raw_input(colored("Do you want to continue as "+spy['name']+" "+spy['salutation']+" or not (Y/N)\n", "red"))
-if choice == "y" or choice == "Y":
-    cprint("Welcome " + spy['name']+" "+spy['salutation'], "blue")
-    while True:
-            choices()
-elif choice == "n" or choice == "N":
-    print "Enter Your Details:"
-    details()
-    choices()
+while True:
+    cprint("Let's Get Started!!", "cyan", attrs=['dark', 'underline', 'bold'])
+    choice = raw_input(colored("Do you want to continue as "+spy['name']+" "+spy['salutation']+" or not (Y/N)\n", "yellow"))
+    if choice == "y" or choice == "Y":
+        cprint("Welcome " + spy['name']+" "+spy['salutation'], "blue")
+        while True:
+                choices()
+    elif choice == "n" or choice == "N":
+        print "Enter Your Details:"
+        details()
+        choices()
+    else:
+        if choice == "e" or choice == "E":
+            cprint("you pressed the quit button \n     GoodBye!!!", "red")
+            exit()
+        cprint("please try again", "red")
+        cprint("For exit press E", "red" , attrs=["bold"])
 
